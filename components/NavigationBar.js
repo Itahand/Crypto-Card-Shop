9f32c5fa654fcfc1ac24b55535295bb6777ef1cb
Noah Naizir <Itahand.naizir@gmail.com>
2023-04-18 10:13:02 -0500

    Walletz

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,26 +1,30 @@
-import Image from "next/image"
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-import * as fcl from "@onflow/fcl"
-import config from "../flow/config.js"
-import publicConfig from "../publicConfig.js"
-import { useEffect, useState } from "react"
+import * as fcl from "@onflow/fcl";
+import config from "../flow/config.js";
+import publicConfig from "../publicConfig.js";
+import { useEffect, useState } from "react";
 
-import { useRecoilState } from "recoil"
+import { useRecoilState } from "recoil";
 import {
   showBasicNotificationState,
   basicNotificationContentState,
-} from "../lib/atoms.js"
-import { LogoutIcon, StarIcon } from "@heroicons/react/outline"
+} from "../lib/atoms.js";
+import { LogoutIcon, StarIcon } from "@heroicons/react/outline";
 
 export default function NavigationBar() {
-  const [, setShowBasicNotification] = useRecoilState(showBasicNotificationState)
-  const [, setBasicNotificationContent] = useRecoilState(basicNotificationContentState)
+  const [, setShowBasicNotification] = useRecoilState(
+    showBasicNotificationState
+  );
+  const [, setBasicNotificationContent] = useRecoilState(
+    basicNotificationContentState
+  );
 
-  const router = useRouter()
-  const [user, setUser] = useState({ loggedIn: null })
-  useEffect(() => fcl.currentUser.subscribe(setUser), [])
+  const router = useRouter();
+  const [user, setUser] = useState({ loggedIn: null });
+  useEffect(() => fcl.currentUser.subscribe(setUser), []);
 
   // useEffect(() => {
   //   const shouldDoConnectionJump = localStorage.getItem("shouldDoConnectionJump") || "YES"
@@ -32,16 +36,27 @@ export default function NavigationBar() {
 
   useEffect(() => {
     window.addEventListener("message", async (d) => {
-      if ((d.data.type === "FCL:VIEW:RESPONSE" && d.data.status === "APPROVED" && (d.data.data.network && d.data.data.network !== publicConfig.chainEnv))
-        || (d.data.type === "LILICO:NETWORK" && typeof d.data.network === "string" && d.data.network != publicConfig.chainEnv)) {
-        fcl.unauthenticate()
-        setShowBasicNotification(true)
-        setBasicNotificationContent({ type: "exclamation", title: "WRONG NETWORK", detail: null })
-        await new Promise(r => setTimeout(r, 2))
-        localStorage.setItem("shouldDoConnectionJump", "YES")
+      if (
+        (d.data.type === "FCL:VIEW:RESPONSE" &&
+          d.data.status === "APPROVED" &&
+          d.data.data.network &&
+          d.data.data.network !== publicConfig.chainEnv) ||
+        (d.data.type === "LILICO:NETWORK" &&
+          typeof d.data.network === "string" &&
+          d.data.network != publicConfig.chainEnv)
+      ) {
+        fcl.unauthenticate();
+        setShowBasicNotification(true);
+        setBasicNotificationContent({
+          type: "exclamation",
+          title: "WRONG NETWORK",
+          detail: null,
+        });
+        await new Promise((r) => setTimeout(r, 2));
+        localStorage.setItem("shouldDoConnectionJump", "YES");
       }
-    })
-  }, [])
+    });
+  }, []);
 
   const AuthedState = () => {
     return (
@@ -52,7 +67,9 @@ export default function NavigationBar() {
           bg-drizzle rounded-full px-3 py-2 leading-5"
           onClick={() => {
             if (user && user.loggedIn) {
-              router.push(`/account/${user.addr}`, undefined, { shallow: true }) 
+              router.push(`/account/${user.addr}`, undefined, {
+                shallow: true,
+              });
             }
           }}
         >
@@ -63,8 +80,9 @@ export default function NavigationBar() {
           type="button"
           className="shrink-0 bg-drizzle rounded-full p-2"
           onClick={() => {
-            router.push(`/bookmark`, undefined, { shallow: true })
-          }}>
+            router.push(`/bookmark`, undefined, { shallow: true });
+          }}
+        >
           <StarIcon className="h-5 w-5 text-black" />
         </button>
 
@@ -72,14 +90,15 @@ export default function NavigationBar() {
           type="button"
           className="shrink-0 bg-drizzle rounded-full p-2"
           onClick={() => {
-            fcl.unauthenticate()
-            localStorage.setItem("shouldDoConnectionJump", "YES")
-          }}>
+            fcl.unauthenticate();
+            localStorage.setItem("shouldDoConnectionJump", "YES");
+          }}
+        >
           <LogoutIcon className="h-5 w-5 text-black" />
         </button>
       </div>
-    )
-  }
+    );
+  };
 
   const UnauthenticatedState = () => {
     return (
@@ -89,12 +108,12 @@ export default function NavigationBar() {
           className="h-12 px-6 text-base rounded-2xl font-flow font-semibold shadow-sm text-black bg-drizzle hover:bg-drizzle-dark"
           onClick={fcl.logIn}
         >
-          <label className="hidden sm:block">Connect Wallet</label>
+          <label className="hidden sm:block">Connect Walletz</label>
           <label className="block sm:hidden">Connect</label>
         </button>
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <div className="px-6 m-auto max-w-7xl min-w-[380px] relative gap-x-5 flex items-center justify-between bg-transparent h-44">
@@ -111,17 +130,14 @@ export default function NavigationBar() {
           </label>
         </Link>
         <label className="hidden sm:block px-1 text-center font-flow text-drizzle font-medium text-xs border border-1 border-drizzle">
-        {`${publicConfig.chainEnv.toUpperCase()}`}
+          {`${publicConfig.chainEnv.toUpperCase()}`}
         </label>
         <label className="block sm:hidden px-1 text-center font-flow text-drizzle font-medium text-xs border border-1 border-drizzle">
-        {`${publicConfig.chainEnv.toUpperCase().charAt(0)}`}
+          {`${publicConfig.chainEnv.toUpperCase().charAt(0)}`}
         </label>
       </div>
 
-      {user && user.loggedIn
-        ? <AuthedState />
-        : <UnauthenticatedState />
-      }
+      {user && user.loggedIn ? <AuthedState /> : <UnauthenticatedState />}
     </div>
-  )
+  );
 }
